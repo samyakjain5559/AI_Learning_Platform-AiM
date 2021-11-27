a2d9d232f6702d44275d9de72bdaae23c887daee
mike-jshen <mike.shen@outlook.com>
2021-11-27 17:45:48 -0500

    port change

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 */
 
-const PORT = process.env.PORT || 4000; // backend routing port
+const PORT = process.env.PORT || 3000; // backend routing port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
