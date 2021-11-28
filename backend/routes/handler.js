13af22b5b1e76c8b7d0ccc507e1a46059b9abf9a
samyak jain <samayak@my.yorku.ca>
2021-11-28 04:56:35 -0500

    Added Teacher and Student login feature

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -20,59 +20,24 @@ router.get('/getsignup', (req, res) => {
     });
 });
 
-router.get('/getlessons', (req, res) => {
-    pool.getConnection( (err, conn) => {
-        if (err) throw err;
-
-        try {
-            conn.query('SELECT * FROM add_lessons', (err, result) => {
-                conn.release();
-                if (err) throw err;
-                res.send(JSON.stringify(result));   // this is rsponse we are sending constantly to this URL
-            });
-        } catch (err) {
-            console.log(err);
-            res.end();
-        }
-    });
-});
-
 router.post('/do_sign_up', (req, res) => {       
     // we are using "req or request(the incoming part)" for reading the data that was send at this /addtweet url from frontend
     const username = req.body.inputemail;
     const userId = 1;
     const userpassword = req.body.inputpassword;
+    const account_type = req.body.checkbox;
 
     pool.getConnection( (err, conn) => {
         if (err) throw err;
 
-        const qry = `INSERT INTO sign_up(person_id, person_email, person_password) VALUES(?,?,?)`;
-        conn.query(qry, [userId, username, userpassword], (err, result) => {
+        const qry = `INSERT INTO sign_up(person_id, person_email, person_password,type) VALUES(?,?,?,?)`;
+        conn.query(qry, [userId, username, userpassword,account_type], (err, result) => {
             conn.release();
             if (err) throw err;
             console.log('user added!');
         });
 
         res.redirect('/');
-        res.end();
-    });
-});
-
-router.post('/do_add_lessons', (req, res) => {       
-    // we are using "req or request(the incoming part)" for reading the data that was send at this /addtweet url from frontend
-    const lesson = req.body.added_lesson;
-
-    pool.getConnection( (err, conn) => {
-        if (err) throw err;
-
-        const qry = `INSERT INTO add_lessons(lessons) VALUES(?)`;
-        conn.query(qry, [lesson], (err, result) => {
-            conn.release();
-            if (err) throw err;
-            console.log('lesson added!');
-        });
-
-        res.redirect('/course_add');
         res.end();
     });
 });
@@ -96,7 +61,12 @@ router.post('/do_login', (req, res) => {
                     console.log( "User found successfully.");
                     currentUser = username;
                     console.log( `Current user is ${currentUser}`);
-                    res.redirect('/addLessons'); 
+                    console.log("Current user is "+ result[0].type);
+                    if(result[0].type == "teacher"){
+                        res.redirect('/addLessons'); 
+                    }else{
+                        res.redirect('/'); // Student page team must redirect to his page
+                    }
                 } else {
                     console.log( "User not found."); 
                     res.redirect('/'); 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import Home from './components/Home';
 import Nav from "./components/Nav";
 import displayit from './components/Test_display';
 import Sign_Up from './components/Sign_Up';
-import CourseAdd from './components/Course_Lessons_add'
 import Adder from './components/Adder';
 import List from './components/List';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
@@ -25,7 +24,6 @@ function App() {
             <Route path="/" exact component={Home} />
             <Route path="/display" exact component={displayit} />
             <Route path="/sign_up" exact component={Sign_Up} />
-            <Route path="/course_add" exact component={CourseAdd} />
             <Route path="/addLessons" exact component={Adder} />
             <Route path="/listLessons" exact component={List} />
           </Switch>
