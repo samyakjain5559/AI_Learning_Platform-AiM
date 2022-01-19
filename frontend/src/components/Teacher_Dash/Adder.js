66fe969ca337ee84daac053f954a1c227a569885
rajakumar-sharujan-216376410 <sharujan@my.yorku.ca>
2022-01-18 19:22:24 -0500

    new changes to adder.js and loginForm.js

--- frontend/src/components/Teacher_Dash/Adder.js
+++ frontend/src/components/Teacher_Dash/Adder.js
@@ -65,7 +65,7 @@ function Adder() {
             </div>
           </div>
         </nav>
-        <h1 class="mt-5">Enter Course information</h1>
+        {/**<h1 class="mt-5">Enter Course information</h1>
         <form method="POST" action="/addCourse">
           <div class="form-row">
             <div class="col">
@@ -115,6 +115,81 @@ function Adder() {
             </div>
           </div>
           <input type="submit" value="Send" class="btn btn-primary mb-2" />
+        </form>**/}
+        <form method="POST" action="/addCourse">
+        <h1 class="mt-5" style={{color: '#7EB10A', textAlign : 'center'}}>Enter Course Information</h1>
+        <br></br>
+        <br></br>
+        <div class="center">
+        <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical" style={{float: 'left', backgroundColor: '#7EB10A'}}>
+          <a class="nav-link active" id="v-pills-courseName-tab" data-toggle="pill" href="#v-pills-courseName" role="tab" aria-controls="v-pills-courseName" aria-selected="true"><h4>Course Name</h4></a>
+          <a class="nav-link" id="v-pills-courseTags-tab" data-toggle="pill" href="#v-pills-courseTags" role="tab" aria-controls="v-pills-courseTags" aria-selected="false"><h4>Course Tags</h4></a>
+          <a class="nav-link" id="v-pills-courseMedia-tab" data-toggle="pill" href="#v-pills-courseMedia" role="tab" aria-controls="v-pills-courseMedia" aria-selected="false"><h4>Course Media</h4></a>
+          <a class="nav-link" id="v-pills-courseImage-tab" data-toggle="pill" href="#v-pills-courseImage" role="tab" aria-controls="v-pills-courseImage" aria-selected="false"><h4>Course Image</h4></a>
+          <a class="nav-link" id="v-pills-courseDescription-tab" data-toggle="pill" href="#v-pills-courseDescription" role="tab" aria-controls="v-pills-courseDescription" aria-selected="false"><h4>Course Description</h4></a>
+          
+        </div>
+        <div class="tab-content" id="v-pills-tabContent" style={{marginLeft: '85px', float: 'left', overflow: 'visible'}}>
+          <div class="tab-pane fade show active" id="v-pills-courseName" role="tabpanel" aria-labelledby="v-pills-courseName-tab" style={{float: 'left'}}>
+          <h4>Enter a Course Name:</h4>
+          <div style={{marginTop: '15px'}}>
+          <input style={{width : '400px'}}
+                name="lessonName"
+                type="text"
+                class="form-control"
+                placeholder="i.e. ENG4000, Introduction to Calculus"
+              />
+          </div>
+          </div>
+          <div class="tab-pane fade" id="v-pills-courseTags" role="tabpanel" aria-labelledby="v-pills-courseTags-tab">
+          <h4>Enter a Course Tag:</h4>
+          <div style={{marginTop: '15px'}}>
+          <input style={{width : '400px'}}
+                name="lessonTag"
+                type="text"
+                class="form-control"
+                placeholder="i.e. Physics, Math, Engineering"
+              />
+          </div>
+          </div>
+          <div class="tab-pane fade" id="v-pills-courseMedia" role="tabpanel" aria-labelledby="v-pills-courseMedia-tab">
+          <h4>Enter a Course Media:</h4>
+          <div style={{marginTop: '15px'}}>
+          <input style={{width : '400px'}}
+                name="lessonMedia"
+                type="text"
+                class="form-control"
+                placeholder="i.e. PDF, MP4, etc."
+              />
+          </div>
+          </div>
+          <div class="tab-pane fade" id="v-pills-courseImage" role="tabpanel" aria-labelledby="v-pills-courseImage-tab">
+          <h4>Enter a Course Image:</h4>
+          <div style={{marginTop: '15px'}}>
+          <input style={{width : '400px'}}
+                name="courseimage"
+                type="text"
+                class="form-control"
+                placeholder="i.e. google image link"
+              />
+          </div>
+          </div>
+          <div class="tab-pane fade" id="v-pills-courseDescription" role="tabpanel" aria-labelledby="v-pills-courseDescription-tab">
+          <h4>Enter a Course Description:</h4>
+          <div style={{marginTop: '15px'}}>
+          <input style={{width : '400px'}}
+                name="coursedescription"
+                type="text"
+                class="form-control"
+                placeholder="i.e. Summary of the course"
+              />  
+          </div>
+          </div>
+        </div>
+        </div>
+        <div style={{marginTop : '250px', marginLeft : '650px'}}>
+          <input style={{width : '200px', fontSize : '20px'}} type="submit" value="Send" class="btn btn-primary mb-2" />
+        </div>
         </form>
         {items.map((item) => (
           <div class="container-fluid p-3 w-50">
