3d18e353f611b96e1e40112bdef372f3d8968ed0
Ruthirakuhan-Varuhn-215634140 <46497611+Ruthirakuhan-Varuhn-215634140@users.noreply.github.com>
2022-03-27 18:49:16 -0400

    Update commentsection.js
    updated commentsection.js

--- commentsection.js
+++ commentsection.js
@@ -1,1 +1,24 @@
+/*
+ This Comment section API is modified from  react-comments-section API.
+ Source : https://www.npmjs.com/package/react-comments-section
+*/
+import React, { useEffect, useState } from "react";
+import data from "./data.json";
+import { Link } from "react-router-dom";
+import { CommentSection } from 'react-comments-section'
 
+const Commentsection = () => {
+    const [comment, setComment] = useState(data)
+    let count = 0
+    comment.map(i => {count+=1; i.replies && i.replies.map(i=> count+=1)} )
+
+    return<div className="commentSection">
+        <div className="header">{count} Comments</div>
+
+        <CommentSection currentUser={"SamJ" && { userId: "SamJ", avatarUrl: "https://ui-avatars.com/api/name=Riya&background=random", name: "Samyak" }} commentsArray={comment}
+        setComment={setComment} signinUrl="./pages/signin" signupUrl="./pages/signin"/>
+        </div>
+    
+}
+
+export default Commentsection;
